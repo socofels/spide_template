2c326dc47f54a07decb05b71713718b17f6f1583
fjh <0>
2021-09-01 17:26:29 +0800

    许可爬虫1.2

--- spide/spide.py
+++ spide/spide.py
@@ -1,3 +1,4 @@
+import threading
 import time, os, requests, bs4
 import pandas as pd
 from threading import Thread, Timer, current_thread
@@ -5,13 +6,7 @@ from queue import Queue
 from tqdm import tqdm
 from fake_useragent import UserAgent
 from units.unit import creat_dir
-
-max_proxies = 50
-q_proxies = Queue(200)
-q_tasks = Queue(max_proxies)
-sleep_time = 1
-running_state = "on running"
-response_list = []
+import logging
 
 
 # [url,header,data,params,task]
@@ -19,16 +14,19 @@ response_list = []
 # *****************************
 # 根据url获取副本的下载链接,然后下载链接
 def func(params, proxies):
-    #  下载副本文件
-    task = params["task"]
-    dataid = task["查看"].split("dataid=")[1]
-    url = "http://permit.mee.gov.cn/perxxgkinfo/syssb/wysb/hpsp/hpsp-company-sewage!showImage.action?dataid=%s" % dataid
-    try:
-        response = requests.get(url=url, headers=params["headers"], proxies=proxies)
-        return response
-    except Exception as e:
-        print(e)
-        return "error"
+    if params == "close":
+        return "close"
+    else:
+        #  下载副本文件
+        task = params["task"]
+        dataid = task["查看"].split("dataid=")[1]
+        url = "http://permit.mee.gov.cn/perxxgkinfo/syssb/wysb/hpsp/hpsp-company-sewage!showImage.action?dataid=%s" % dataid
+        try:
+            response = requests.get(url=url, headers=params["headers"], proxies=proxies)
+            return response
+        except Exception as e:
+            print(e)
+            return "error"
 
 
 def validation(response):
@@ -38,6 +36,18 @@ def validation(response):
         return "Unexpected response"
     else:
         return True
+
+
+def task_manage(thread_list):
+    global running_state, q_tasks
+    while running_state == "on running" or not q_tasks.empty():
+        task_list = pd.DataFrame(columns=["线程", "状态", "运行时长", "已完成任务数"])
+        for thread in thread_list:
+            task_list=task_list.append({"线程": thread.ident, "状态": thread.is_alive(), "运行时长": thread.end_time - thread.start_time,
+                              "已完成任务数": thread.completed}, True)
+
+        print("\r%s"%task_list)
+        time.sleep(1)
 
 
 # ******************************
@@ -53,10 +63,13 @@ def start(proxies_url, thread_num):
         thread_list.append(ThreadTask(func, validation, accept_task(), accept_proxies()))
     for i in thread_list:
         i.start()
+    task_manager=Thread(target=task_manage,name="task_manager",args=[thread_list])
+    task_manager.start()
     for i in thread_list:
         i.join()
     task_publisher.join()
     proxies_publisher.join()
+    task_manager.join()
     print("well done")
 
 
@@ -66,18 +79,19 @@ def get_task():
     """获取任务,每隔0.1秒检测一次是否需要添加新任务"""
     global q_tasks, running_state
     task_list = pd.read_csv(r"C:\Users\fjh\PycharmProjects\spide_template\data\许可信息公开.csv")
+    # task_list = pd.read_csv(r"C:\Users\fjh\PycharmProjects\spide_template\data\许可信息公开0.csv")
 
-    print(task_list)
+    # print(task_list)
     for i in task_list.index:
         if task_list.loc[i, "副本下载状态"] == 0:
             while True:
                 if not q_tasks.full():
-                        new_serise = {}
-                        new_serise["task"] = dict(task_list.loc[i])
-                        new_serise["url"] = "http://permit.mee.gov.cn" + new_serise["task"]["查看"]
-                        new_serise["headers"] = {'user-agent': UserAgent().random}
-                        q_tasks.put(new_serise)
-                        break
+                    new_serise = {}
+                    new_serise["task"] = dict(task_list.loc[i])
+                    new_serise["url"] = "http://permit.mee.gov.cn" + new_serise["task"]["查看"]
+                    new_serise["headers"] = {'user-agent': UserAgent().random}
+                    q_tasks.put(new_serise)
+                    break
                 else:
                     time.sleep(0.1)
     running_state = "close"
@@ -100,12 +114,11 @@ def get_proxies(proxies_url):
     flag = True
     while flag:
         if q_proxies.empty():
-            print(proxies_url)
             try:
                 proxies_list = request_proxies(proxies_url)
                 for i in proxies_list.index:
-                    proxy={"http": "http://%(ip)s:%(port)s" % proxies_list.loc[i,"data"]}
-                    print(proxy)
+                    proxy = {"http": "http://%(ip)s:%(port)s" % proxies_list.loc[i, "data"]}
+                    # print(proxy)
                     q_proxies.put(proxy)
             except:
 
@@ -116,6 +129,7 @@ def get_proxies(proxies_url):
 # 返回一个proxies
 def accept_proxies():
     while q_proxies.empty():
+        # print("没有代理")
         time.sleep(0.1)
     return q_proxies.get()
 
@@ -137,6 +151,9 @@ class ThreadTask(Thread):
         self.validation = validation
         self.task = task
         self.proxy = proxy
+        self.start_time = time.time()
+        self.end_time = time.time()
+        self.completed = 0
 
     def run(self):
         # flag表示任务是否继续,当flag==False表示没有更多任务了
@@ -154,57 +171,71 @@ class ThreadTask(Thread):
                 err_time = 0
                 while err_time < 10:
                     try:
-                        print("执行任务")
+                        # print("执行任务")
                         result = self.func(self.task, self.proxy)
+                        if result == "close":
+                            flag = False
+                            break
                         val = self.validation(result)
-                        print(val)
+                        # print(val)
                         # validation应当给出三种结论代理失效,非正常页面,正常
                         if val == "Expired proxy" or val == "Unexpected response":
                             err_time = err_time + 1
                             self.proxy = accept_proxies()
                         else:
-                            print("没问题")
+                            # print("没问题")
                             err_time = 0
                             # todo 修改为需要的操作--------
                             response = result
                             content = bs4.BeautifulSoup(response.text, "html.parser")
                             imageDiv = content.find("body").find("div", id="imageDiv")
-                            print("body")
+                            # print("body")
                             imgCount = imageDiv.find("input", id="imgCount").get("value")
-                            print("input")
+                            # print("input")
                             pkid = imageDiv.find("input", id="pkid").get("value")
-                            print("input")
+                            # print("input")
 
                             dataid = self.task["task"]["查看"].split("dataid=")[1]
-                            print("没问题")
-                            print(imgCount)
+                            # print("没问题")
+                            # print(imgCount)
                             for i in range(1, int(imgCount) + 1):
-                                print(dataid)
                                 url = f"http://permit.mee.gov.cn/perxxgkinfo/syssb/xkgg/xkgg!downFilePng.action?datafileid={pkid}_{i}&fileType=pdffile&dataid={dataid}"
                                 # print(url)
                                 err_flag = 0
                                 while err_flag < 10:
+                                    self.end_time=time.time()
                                     try:
-                                        print(url)
+                                        logging.debug(url)
                                         img = requests.get(url, headers=self.task["headers"], proxies=self.proxy)
                                     except Exception as e:
                                         print(e)
                                         img = "error"
+                                    finally:
+                                        self.end_time = time.time()
                                     if validation(img) == "Expired proxy" or val == "Unexpected response":
                                         err_flag = err_flag + 1
                                         self.proxy = accept_proxies()
                                     else:
-                                        # 下载图片
-                                        print("下载图片")
-                                        data = self.task["task"]
-                                        index = data["index"]
-                                        category = data["行业类别"]
-                                        name = data["单位名称"]
-                                        unique_id = data["许可证编号"]
-                                        path = f'../data/许可信息公开/{category}/{name}{unique_id}/许可证副本/'
-                                        creat_dir(path)
-                                        with open(path + str(i) + ".png", 'wb') as f:
-                                            f.write(img.content)
+                                        while True:
+                                            self.end_time = time.time()
+                                            # 下载图片
+                                            # print("下载图片")
+                                            data = self.task["task"]
+                                            index = data["index"]
+                                            category = data["行业类别"]
+                                            name = data["单位名称"]
+                                            unique_id = data["许可证编号"]
+                                            path = f'../data/许可信息公开/{category}/{name}{unique_id}/许可证副本/'
+                                            creat_dir(path)
+                                            file_path = path + str(i) + ".png"
+                                            with open(file_path, 'wb') as f:
+                                                f.write(img.content)
+                                            if os.path.getsize(file_path) == 520:
+                                                self.proxy = accept_proxies()
+                                            else:
+                                                # 准确无误的下载了一个文件
+                                                self.completed += 1
+                                                break
                                         break
                         # -------------------
                         self.task = accept_task()
@@ -212,14 +243,24 @@ class ThreadTask(Thread):
                         print(e)
                         err_time = err_time + 1
                         self.proxy = accept_proxies()
+                    finally:
+                        self.end_time = time.time()
                 # 更新任务
                 task = accept_task()
-                if task == "clsoe":
+                if task == "close":
                     flag = False
                 time.sleep(sleep_time)
+        print("线程 %s 已完成所有任务" % threading.currentThread().name)
 
 
 if "__main__" == __name__:
-    proxies_url = "http://tiqu.pyhttp.taolop.com/getip?count=50&neek=8887&type=2&yys=0&port=1&sb=&mr=1&sep=0&ts=1&time=2"
-    thred_num = 50
+    max_proxies = 1
+    q_proxies = Queue(200)
+    q_tasks = Queue(max_proxies)
+    sleep_time = 1
+    running_state = "on running"
+    response_list = []
+    logging.debug("start")
+    proxies_url = "http://tiqu.pyhttp.taolop.com/getip?count=1&neek=8887&type=2&yys=0&port=1&sb=&mr=1&sep=0&ts=1&time=2"
+    thred_num = 2
     start(proxies_url, thred_num)
--- spide/tests/test_main.py
+++ spide/tests/test_main.py
@@ -1,35 +1,50 @@
-from spide.units.unit import getHeader, xunfei_orc, turn_to_list
+# from spide.units.unit import getHeader, xunfei_orc, turn_to_list
 import pandas as pd
 import os
 
 path = "F:\PL\ssl\spider_排污许可"
 
-
 # 遍历文件夹，修改下载状态
-def detect():
-    header = getHeader()
-    download_list = pd.DataFrame(columns=["path", "status", "content"])
-    download_list.loc[0] = ["path", "status", "content"]
-    xxgk = pd.read_csv("F:/PL/ssl/spider_排污许可/data/许可信息公开.csv")
-    for i in xxgk.index:
-        path = f"F:/PL/ssl/spider_排污许可/许可信息公开/{xxgk.loc[i, '行业类别']}/{xxgk.loc[i, '单位名称']}{xxgk.loc[i, '许可证编号']}/许可证副本"
-        if os.path.exists(path + "/5.png"):
-            if not os.path.getsize(path + "/5.png") == 520:
-                content = turn_to_list(xunfei_orc(path + "/5.png"))
-                download_list.loc[download_list.index[-1]+1]=[path,0,content]
-                download_list.to_csv(r"F:\PL\ssl\spider_排污许可\data\副本orc识别信息.csv")
+# def detect():
+#     header = getHeader()
+#     download_list = pd.DataFrame(columns=["path", "status", "content"])
+#     download_list.loc[0] = ["path", "status", "content"]
+#     xxgk = pd.read_csv("F:/PL/ssl/spider_排污许可/data/许可信息公开.csv")
+#     for i in xxgk.index:
+#         path = f"F:/PL/ssl/spider_排污许可/许可信息公开/{xxgk.loc[i, '行业类别']}/{xxgk.loc[i, '单位名称']}{xxgk.loc[i, '许可证编号']}/许可证副本"
+#         if os.path.exists(path + "/5.png"):
+#             if not os.path.getsize(path + "/5.png") == 520:
+#                 content = turn_to_list(xunfei_orc(path + "/5.png"))
+#                 download_list.loc[download_list.index[-1]+1]=[path,0,content]
+#                 download_list.to_csv(r"F:\PL\ssl\spider_排污许可\data\副本orc识别信息.csv")
 
 
-import requests
-from fake_useragent import UserAgent
+# import requests
+# from fake_useragent import UserAgent
+#
+# try:
+#
+#     headers={"User-Agent":UserAgent().random}
+#     proxies={"http":None,"https":None}
+#     print(headers)
+#     res = requests.get(url="http://www.csres.com/sort/industry.jsp",headers=headers, timeout=3,proxies=proxies)
+#
+#
+# except Exception as e:
+#     print(e)
 
-try:
 
-    headers={"User-Agent":UserAgent().random}
-    proxies={"http":None,"https":None}
-    print(headers)
-    res = requests.get(url="http://www.csres.com/sort/industry.jsp",headers=headers, timeout=3,proxies=proxies)
+import time
+# import pandas as pd
+import os
+import sys
 
-
-except Exception as e:
-    print(e)
+for i in range(0, 10):
+    data = pd.DataFrame(data=[[1, 2, 3]], columns=["a", "b", "c"])
+    # pd.set_option('expand_frame_repr', False)
+    # print ("\r\r%s"%data)
+    # sys.stdout.flush()
+    # os.system("cls")
+    print("%s" % data, end="", flush=True)
+    # print("\ri am ok",flush=True)
+    time.sleep(1)
